42cdde2b9dd952c96caf5bc6d684d64190e41280
Christopher <christopher.nublado@gmail.com>
2022-03-09 06:18:28 -0800

    ...

--- nublado/nublado_bot/bot_commands/group_admin.py
+++ nublado/nublado_bot/bot_commands/group_admin.py
@@ -1,6 +1,10 @@
 import logging
 
-from telegram import Update, User as TelegramUser
+from telegram import (
+    Update, User as TelegramUser,
+    InlineKeyboardButton, InlineKeyboardMarkup
+)
+
 from telegram.ext import (
     CallbackContext, MessageHandler, Filters
 )
@@ -17,10 +21,23 @@ from django_telegram.bot_utils.user_status import (
     restricted_group_owner
 )
 from django_telegram.models import TelegramGroupMember
+from language_days.functions import set_language_day_locale
 
 logger = logging.getLogger('django')
 
 GROUP_ID = settings.NUBLADO_GROUP_ID
+
+welcome_message = _(
+    "Welcome to the group, {name}.\n\n" \
+    "Please read the following rules and click the \"I agree\" button to participate.\n\n" \
+    "*Rules (tentative)*\n" \
+    "- Communicate in only English and Spanish.\n" \
+    "- Don't harass other group members.\n" \
+    "- Don't send private messages to other group members without their permission.\n" \
+    "- Be a good example. There are people here learning your language. Help them out with corrections."
+)
+
+agree_message = _("I agree.")
 
 
 @send_typing_action
@@ -52,13 +69,22 @@ def update_group_members(update: Update, context: CallbackContext) -> None:
 
 def member_join(update: Update, context: CallbackContext) -> None:
     if update.message.new_chat_members:
+        set_language_day_locale()
         for user in update.message.new_chat_members:
-            message = _("Welcome to the group, {name}.").format(
+            message = _(welcome_message).format(
                 name=user.mention_markdown()
             )
+            keyboard = [
+                [
+                    InlineKeyboardButton(_(agree_message), callback_data='1'),
+                ],
+            ]
+
+            reply_markup = InlineKeyboardMarkup(keyboard)
             context.bot.send_message(
                 chat_id=GROUP_ID,
-                text=message
+                text=message,
+                reply_markup=reply_markup
             )
         # Delete service message.
         try:
@@ -82,12 +108,14 @@ def member_exit(update: Update, context: CallbackContext) -> None:
             pass
 
 
+# Listen for when new members join group.
 member_join_handler = MessageHandler(
     Filters.status_update.new_chat_members,
     member_join
 )
 
 
+# Listen for when members leave group.
 member_exit_handler = MessageHandler(
     Filters.status_update.left_chat_member,
     member_exit
