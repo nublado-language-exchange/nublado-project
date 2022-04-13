3d0a5876d4d2a73e1d74e2270a134e6053a96d4e
Christopher <christopher.nublado@gmail.com>
2022-04-13 07:44:18 -0700

    ...

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 release: python manage.py migrate
 web: gunicorn config.wsgi --workers $WEB_CONCURRENCY --pythonpath $PYTHONPATH --log-file -
-# worker: python manage.py runbot nubladobot
--- nublado/project_app/apps.py
+++ nublado/project_app/apps.py
@@ -10,12 +10,14 @@ class ProjectAppConfig(AppConfig):
     name = "project_app"
 
     def ready(self):
-        from django_telegram.apps import DjangoTelegramConfig
-        try:
-            bot_token = settings.DJANGO_TELEGRAM['bots'][settings.NUBLADO_BOT]['token']
-            bot = DjangoTelegramConfig.bot_registry.get_bot(bot_token)
-            if bot:
-                bot.start()
-        except:
-            error = "Bot {} doesn't exist or is improperly configured.".format(settings.NUBLADO_BOT)
-            logger.error(error)
+        dt = settings.DJANGO_TELEGRAM
+        if dt['mode'] == settings.BOT_MODE_WEBHOOK:
+            from django_telegram.apps import DjangoTelegramConfig
+            try:
+                bot_token = settings.DJANGO_TELEGRAM['bots'][settings.NUBLADO_BOT]['token']
+                bot = DjangoTelegramConfig.bot_registry.get_bot(bot_token)
+                if bot:
+                    bot.start()
+            except:
+                error = "Bot {} doesn't exist or is improperly configured.".format(settings.NUBLADO_BOT)
+                logger.error(error)
